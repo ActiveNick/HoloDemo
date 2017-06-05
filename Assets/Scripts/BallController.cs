f5a347e4d6e2b00392202c1d8ed0107b86de3151
Nick Landry <nick@mobility42.com>
2017-06-04 21:19:29 -0400

    Initial commit

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    private Rigidbody body;
+
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody>();
+	}
+
+    public void EnableGravity()
+    {
+        body.useGravity = true;
+    }
+
+    public void DisableGravity()
+    {
+        body.useGravity = false;
+    }
+
+    public void ResetPosition()
+    {
+        gameObject.transform.localPosition = Vector3.zero;
+        body.velocity = Vector3.zero;
+        body.angularVelocity = Vector3.zero;
+    }
+}
